1be410ef378fa33de5c47a9181ecb3395b7e77ae
tinnerrss <tinnerrss@hotmail.com>
2020-02-06 13:49:04 -0800

    did I even do anything

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-let startButton = document.getElementById("startButton").addEventListener("click", function() {
-    document.getElementById("introContainer").style.display = "none";
+
+let startButton = document.getElementById("startButton");startButton.addEventListener("click", function() {
+    document.getElementById("popUpContainer").style.display = "none";
 })
+let nextLevelButton = document.getElementById("nextLevel");nextLevelButton.style.display = "none";
 
 let game = document.getElementById("game");
 let ctx = game.getContext("2d");
@@ -8,56 +10,68 @@ let ctx = game.getContext("2d");
 game.setAttribute("width", getComputedStyle(game)["width"]);
 game.setAttribute("height", getComputedStyle(game)["height"]);
 
+let creepImage1 = new Image();
+creepImage1.src = "https://vignette.wikia.nocookie.net/johnnybravo/images/b/bb/Johnnyb001.gif/revision/latest/scale-to-width-down/340?cb=20190421193227";
+let creepImage2 = new Image();
+creepImage2.src = "https://media.giphy.com/media/l1J9ASb54AziiHSVi/giphy.gif";
+let playerImage = new Image();
+playerImage.src = "https://cdn4.iconfinder.com/data/icons/faces-10/96/Smiley-512.png";
 
-function Thing(x, y, color, width, height) {
+
+let level = 1;
+
+
+
+//function to create objects with images
+function Thing(image, x, y, color, width, height) {
     this.x = x;
     this.y = y;
     this.color = color;
     this.width = width;
     this.height = height;
+    this.image = image;
     this.alive = true;
     this.hit = true;
     this.render = function() {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.clearRect(0, 0, this.width, this.height);
+        ctx.drawImage(this.image, this.x, this.y, this.width,this.height);
     }
 }
-// console.log(game.width)
-// console.log(game.height)
 
-let player = new Thing(50, 450, "white", 30, 30);
-
+function WallThing(x, y, color, width, height) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.width = width;
+    this.height = height;
+    this.hit = true;
+    this.render = function() {
+        ctx.fillStyle = this.color;
+        ctx.clearRect(0, 0, this.width, this.height);
+        ctx.fillRect(this.x, this.y, this.width,this.height);
+    }
+}
+//function to create object that are colored rectangles
+let player = new Thing(playerImage, 700, 220, "white", 30, 30);
 // Creating Creeps
 let creeps = [
-    creep1 = new Thing(380, 70, "red", 30, 30),
-    creep2 = new Thing(150, 350, "blue", 30, 30),
-    creep3 = new Thing(250, 150, "green", 30, 30),
-    creep4 = new Thing(360, 400, "gray", 30, 30),
-    creep5 = new Thing(650, 150, "hotpink", 30, 30),
-    creep6 = new Thing(700, 430, "orange", 30, 30),
-    creep7 = new Thing(490, 300, "yellow", 30, 30)
-    // creep5 = new Thing(500, 0, "red", 30, 30),
-    // creep5 = new Thing(500, 0, "red", 30, 30),
-    // creep5 = new Thing(500, 0, "red", 30, 30),
-    // creep5 = new Thing(500, 0, "red", 30, 30),
+    creep1 = new Thing(creepImage1, 380, 70, "red", 30, 30),
+    creep2 = new Thing(creepImage1, 150, 350, "blue", 30, 30),
+    creep3 = new Thing(creepImage1, 250, 150, "green", 30, 30),
+    creep4 = new Thing(creepImage1, 360, 400, "gray", 30, 30),
+    creep5 = new Thing(creepImage1, 650, 150, "hotpink", 30, 30),
+    creep6 = new Thing(creepImage1, 700, 430, "orange", 30, 30),
+    creep7 = new Thing(creepImage1, 490, 300, "yellow", 30, 30)
 ] 
-// let creeps = [];
-// for(let i = 0; i < 10; i++){
-    
-//     creeps[i] = new Thing(Math.floor(Math.random()*(game.width-20)), Math.floor(Math.random()*(game.height-20)), "red", 30, 30);
-    // TODO: write creep dance function which makes them move circularly
-// }
 //creating an array of wall
 let walls = [
-    wall1 = new Thing(0, 200, "black", 180, 50),
-    wall2 = new Thing(640, 350, "black", 50, 180),
-    wall3 = new Thing(700, 250, "black", 180, 50),
-    wall4 = new Thing(300, 200, "black",50, 300),
-    wall5 = new Thing(500, 0, "black", 50, 250)
+    wall1 = new WallThing(0, 200, "black", 180, 50),
+    wall2 = new WallThing(640, 350, "black", 50, 180),
+    wall3 = new WallThing(700, 250, "black", 180, 50),
+    wall4 = new WallThing(300, 200, "black",50, 300),
+    wall5 = new WallThing(500, 0, "black", 50, 250)
 ] 
-
-
-let finishLine = new Thing(700, 0, "green", 200, 100);
+let finishLine = new WallThing(700, 0, "gray", 200, 100);
 
 
 let moveCount = 0;
@@ -79,28 +93,67 @@ function movementHandler(e) {
         case (87):
             //if player.y is negative then dont move up
             // if (!checkWallCollision(87)) { move up }
-            if (!(player.y <= 0) && checkWallCollision())  {
+            if (!(player.y <= 0))  {
                 player.y -=10;
             }
             break;
         case (65):
-            if (!(player.x <= 0) && checkWallCollision()) {
+            if (!(player.x <= 0)) {
                 player.x -=10;
             }
             break;
         case (83):
-            if (!(player.y >= 475) && checkWallCollision()) {
+            if (!(player.y >= 475)) {
                 player.y +=10;
             }
             break;
         case (68):
-            if (!(player.x >= 970) && checkWallCollision()) {
+            if (!(player.x >= 970)) {
                 player.x +=10;   
             }
     }
 }
 
+                
 document.addEventListener("keydown", movementHandler);
+
+function gameInit() {
+    console.log("gameinit working?")
+    player.alive = true;
+    player.render();
+    creeps.forEach(function(creep) {
+        creep.alive = true;
+        creep.render();
+    })
+    walls.forEach(function(wall) {
+        wall.render();
+    })
+    finishLine.render();
+    gameLoop();
+    setInterval(runGame);
+}
+
+function win() {
+    document.getElementById("popUpContainer").style.display = "initial";
+    document.getElementById("popUpTitle").innerText = "You Made It!";
+    document.getElementById("intro").innerText = "Yaaayyyy you dogded the creeps and found your friend! Now she wants to go to the next location.";
+    //add dancing tina
+    nextLevelButton.style.display = "initial";
+    startButton.style.display = "none";
+    nextLevelButton.addEventListener("click", function() {
+        ctx.clearRect(0, 0, game.width, game.height);
+    })
+}
+
+function endGame() {
+    document.getElementById("popUpContainer").style.display = "initial";
+    document.getElementById("popUpTitle").innerText = "Oh no!";
+    document.getElementById("intro").innerText = "Unfortunately you ran into a creep and they're not letting you leave their sight! Better luck next time.";
+    startButton.innerText = "RESET";
+    startButton.addEventListener("click", function() {
+        location.reload();
+    })
+}
 //Detect when play runs into creeps
 function detectCreepHit() {
     creeps.forEach(function(creep) {
@@ -109,50 +162,80 @@ function detectCreepHit() {
             player.y < creep.y + creep.height &&
             player.y + player.height > creep.y) {
                 player.alive = false;
-            }
-                //run end game function
+                endGame();
+                clearInterval(runGame);
+               
+            }          //run end game function
          })  
 }  
-// Detect when player runs into walls
-function checkWallCollision() {
-    // switch statement checking direction
-    var isItTrue = true
-    walls.forEach(function (wall) {
-        if (player.x < wall.x + wall.width &&
-            player.x + player.width > wall.x &&
-            player.y < wall.y + wall.height &&
-            player.y + player.height > wall.y) {
-                isItTrue = false;
-             } 
-        })
-    return isItTrue
-}
 
-function gameLoop() {
-    
+function checkWin() {
+     if (player.x < finishLine.x + finishLine.width &&
+        player.x + player.width > finishLine.x &&
+        player.y < finishLine.y + finishLine.height &&
+        player.y + player.height > finishLine.y) {
+            
+            win();
+            clearInterval(runGame);
+    }
+}  
+// Detect when player runs into walls
+// function checkWallCollision() {
+//     walls.forEach(function (wall) {
+//         switch (e.keycode) {
+//             case (87):
+//                 if (player.y < wall.y + wall.height &&
+//                     player.y + player.height > wall.y) {
+//                         return false;
+//                     }
+                
+//                 break;
+//             case (65):
+//                 if (player.x < wall.x + wall.width &&
+//                     player.x + player.width > wall.x) {
+//                         return false;
+//                     }
+
+//                 break;
+//             case (83):
+//                 if (player.y < wall.y + wall.height &&
+//                     player.y + player.height > wall.y) {
+//                         return false;
+//                     }
+
+//                 break;
+//             case (68):
+//                 if (player.x < wall.x + wall.width &&
+//                     player.x + player.width > wall.x) {
+//                         return false;
+//                     }
+
+//         } 
+//     })
+// } 
+
+
+function gameLoop() {  
     ctx.clearRect(0, 0, game.width, game.height);
-  if(player.alive) {
-      player.render();
-  }
-   
+    if(player.alive) {
+        player.render();
+    }
     creeps.forEach(function(creep){
-        if(creep.alive){
+        // if(creep.alive){
             dancingCreeps();
             creep.render();
-        } 
+        // } 
         detectCreepHit();
     })
     walls.forEach(function(wall){
         wall.render();
+        
     })
-    // wall1.render();
-    // wall2.render();
-    // wall3.render();
-    // wall4.render();
-    // wall5.render();
-    checkWallCollision();
     finishLine.render();
+    checkWin();
     moveCount++;
 }
+
+
 let runGame = setInterval(gameLoop, 100);
 
