ee8420cbfa15b06105d19ccd740f4d936345de4b
tinnerrss <tinnerrss@hotmail.com>
2020-02-06 17:02:28 -0800

    yay we can move up!

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ game.setAttribute("height", getComputedStyle(game)["height"]);
 let creepImage1 = new Image();
 creepImage1.src = "https://vignette.wikia.nocookie.net/johnnybravo/images/b/bb/Johnnyb001.gif/revision/latest/scale-to-width-down/340?cb=20190421193227";
 let creepImage2 = new Image();
-creepImage2.src = "https://media.giphy.com/media/l1J9ASb54AziiHSVi/giphy.gif";
+creepImage2.src = "https://s.tcdn.co/8b8/ab8/8b8ab835-6e72-4fee-8340-73dba6d204d8/1.png";
 let playerImage = new Image();
 playerImage.src = "https://cdn4.iconfinder.com/data/icons/faces-10/96/Smiley-512.png";
 
 
-let level = 1;
+// let level = 1;
 
 
 
@@ -51,7 +51,7 @@ function WallThing(x, y, color, width, height) {
         ctx.fillRect(this.x, this.y, this.width,this.height);
     }
 }
-//function to create object that are colored rectangles
+// function to create object that are colored rectangles
 let player = new Thing(playerImage, 700, 220, "white", 30, 30);
 // Creating Creeps
 let creeps = [
@@ -92,8 +92,8 @@ function movementHandler(e) {
     switch (e.keyCode) {
         case (87):
             //if player.y is negative then dont move up
-            // if (!checkWallCollision(87)) { move up }
-            if (!(player.y <= 0))  {
+            // if (!canMove(87)) { move up }
+            if (!(player.y <= 0) && canMove(87))  {
                 player.y -=10;
             }
             break;
@@ -114,6 +114,27 @@ function movementHandler(e) {
     }
 }
 
+// Detect when player runs into walls
+function canMove(keycode) {
+    // Check if player can move, if so, return true
+    let returnValue = true;
+    switch (keycode) {
+        case 87:
+            // up
+            for (let i = 0; i < walls.length; i++) {
+                if (player.x < (walls[i].x + walls[i].width) && (player.x + player.width) > walls[i].x){
+                    if (player.y === (walls[i].y + walls[i].height)) {
+                        console.log('Cannot move up');
+                        returnValue = false;
+                        return;
+                    }
+                }
+            }
+            break;
+    }
+    console.log('out of switch')
+    return returnValue;
+} 
                 
 document.addEventListener("keydown", movementHandler);
 
@@ -141,7 +162,9 @@ function win() {
     nextLevelButton.style.display = "initial";
     startButton.style.display = "none";
     nextLevelButton.addEventListener("click", function() {
+        document.getElementById("popUpContainer").style.display = "none";
         ctx.clearRect(0, 0, game.width, game.height);
+        level++;
     })
 }
 
@@ -149,7 +172,7 @@ function endGame() {
     document.getElementById("popUpContainer").style.display = "initial";
     document.getElementById("popUpTitle").innerText = "Oh no!";
     document.getElementById("intro").innerText = "Unfortunately you ran into a creep and they're not letting you leave their sight! Better luck next time.";
-    startButton.innerText = "RESET";
+    startButton.innerText = "START OVER";
     startButton.addEventListener("click", function() {
         location.reload();
     })
@@ -179,40 +202,6 @@ function checkWin() {
             clearInterval(runGame);
     }
 }  
-// Detect when player runs into walls
-// function checkWallCollision() {
-//     walls.forEach(function (wall) {
-//         switch (e.keycode) {
-//             case (87):
-//                 if (player.y < wall.y + wall.height &&
-//                     player.y + player.height > wall.y) {
-//                         return false;
-//                     }
-                
-//                 break;
-//             case (65):
-//                 if (player.x < wall.x + wall.width &&
-//                     player.x + player.width > wall.x) {
-//                         return false;
-//                     }
-
-//                 break;
-//             case (83):
-//                 if (player.y < wall.y + wall.height &&
-//                     player.y + player.height > wall.y) {
-//                         return false;
-//                     }
-
-//                 break;
-//             case (68):
-//                 if (player.x < wall.x + wall.width &&
-//                     player.x + player.width > wall.x) {
-//                         return false;
-//                     }
-
-//         } 
-//     })
-// } 
 
 
 function gameLoop() {  
@@ -239,3 +228,179 @@ function gameLoop() {
 
 let runGame = setInterval(gameLoop, 100);
 
+
+//-----------------------------Adding Levels?!--------------------------//
+
+// let runGame2 = setInterval(theGame, 100);
+
+
+
+// function theGame() {
+//     let level = 1;
+//     let moveCount = 0;
+//     let creepImage1 = new Image();
+//     creepImage1.src = "https://vignette.wikia.nocookie.net/johnnybravo/images/b/bb/Johnnyb001.gif/revision/latest/scale-to-width-down/340?cb=20190421193227";
+//     let creepImage2 = new Image();
+//     creepImage2.src = "https://s.tcdn.co/8b8/ab8/8b8ab835-6e72-4fee-8340-73dba6d204d8/1.png";
+//     let playerImage = new Image();
+//     playerImage.src = "https://cdn4.iconfinder.com/data/icons/faces-10/96/Smiley-512.png";
+
+//     //if count is even move right or else move left if count is odd
+//     function dancingCreeps() {
+//         if(moveCount % 10 === 0){
+//             creeps.forEach(function(creep) {
+//                 creep.x +=5;
+//             })
+//         } else if (moveCount % 10 === 5 ) {
+//             creeps.forEach(function(creep) {
+//                 creep.x -=5;
+//             })
+//         }
+//     }
+    
+//     function movementHandler(e) {
+//         switch (e.keyCode) {
+//             case (87):
+//                 //if player.y is negative then dont move up
+//                 // if (!canMove(87)) { move up }
+//                 if (!(player.y <= 0))  {
+//                     player.y -=10;
+//                 }
+//                 break;
+//             case (65):
+//                 if (!(player.x <= 0)) {
+//                     player.x -=10;
+//                 }
+//                 break;
+//             case (83):
+//                 if (!(player.y >= 475)) {
+//                     player.y +=10;
+//                 }
+//                 break;
+//             case (68):
+//                 if (!(player.x >= 970)) {
+//                     player.x +=10;   
+//                 }
+//         }
+//     }
+    
+                    
+//     document.addEventListener("keydown", movementHandler);
+
+
+//     function gameLoop() {  
+//         ctx.clearRect(0, 0, game.width, game.height);
+//         if(player.alive) {
+//             player.render();
+//         }
+//         creeps.forEach(function(creep){
+//             // if(creep.alive){
+//                 dancingCreeps();
+//                 creep.render();
+//             // } 
+//             detectCreepHit();
+//         })
+//         walls.forEach(function(wall){
+//             wall.render();
+            
+//         })
+//         finishLine.render();
+//         checkWin();
+//         moveCount++;
+//     }
+
+//     function checkWin() {
+//         if (player.x < finishLine.x + finishLine.width &&
+//            player.x + player.width > finishLine.x &&
+//            player.y < finishLine.y + finishLine.height &&
+//            player.y + player.height > finishLine.y) {
+               
+//                win();
+//                clearInterval(runGame);
+//        }
+//    }  
+
+//    function detectCreepHit() {
+//     creeps.forEach(function(creep) {
+//         if (player.x < creep.x + creep.width &&
+//             player.x + player.width > creep.x &&
+//             player.y < creep.y + creep.height &&
+//             player.y + player.height > creep.y) {
+//                 player.alive = false;
+//                 endGame();
+//                 clearInterval(runGame);
+               
+//             }          //run end game function
+//          })  
+// }  
+
+//     function endGame() {
+//         document.getElementById("popUpContainer").style.display = "initial";
+//         document.getElementById("popUpTitle").innerText = "Oh no!";
+//         document.getElementById("intro").innerText = "Unfortunately you ran into a creep and they're not letting you leave their sight! Better luck next time.";
+//         startButton.innerText = "START OVER";
+//         startButton.addEventListener("click", function() {
+//             location.reload();
+//         })
+//     }
+
+//     function win() {
+//         document.getElementById("popUpContainer").style.display = "initial";
+//         document.getElementById("popUpTitle").innerText = "You Made It!";
+//         document.getElementById("intro").innerText = "Yaaayyyy you dogded the creeps and found your friend! Now she wants to go to the next location.";
+//         //add dancing tina
+//         nextLevelButton.style.display = "initial";
+//         startButton.style.display = "none";
+//         nextLevelButton.addEventListener("click", function() {
+//             document.getElementById("popUpContainer").style.display = "none";
+//             ctx.clearRect(0, 0, game.width, game.height);
+//             level++;
+//         })
+//     }
+
+
+
+//     if (level = 1) {
+//         let player = new Thing(playerImage, 700, 220, "white", 30, 30);
+//         let creeps = [
+//         creep1 = new Thing(creepImage1, 380, 70, "red", 30, 30),
+//         creep2 = new Thing(creepImage1, 150, 350, "blue", 30, 30),
+//         creep3 = new Thing(creepImage1, 250, 150, "green", 30, 30),
+//         creep4 = new Thing(creepImage1, 360, 400, "gray", 30, 30),
+//         creep5 = new Thing(creepImage1, 650, 150, "hotpink", 30, 30),
+//         creep6 = new Thing(creepImage1, 700, 430, "orange", 30, 30),
+//         creep7 = new Thing(creepImage1, 490, 300, "yellow", 30, 30)
+//         ] 
+//         let walls = [
+//         wall1 = new WallThing(0, 200, "black", 180, 50),
+//         wall2 = new WallThing(640, 350, "black", 50, 180),
+//         wall3 = new WallThing(700, 250, "black", 180, 50),
+//         wall4 = new WallThing(300, 200, "black",50, 300),
+//         wall5 = new WallThing(500, 0, "black", 50, 250)
+//         ] 
+//         let finishLine = new WallThing(700, 0, "gray", 200, 100);
+//         gameLoop();
+//     } else if (level = 2) {
+//         let player = new Thing(playerImage, 700, 220, "white", 30, 30);
+//         let creeps = [
+//         creep1 = new Thing(creepImage2, 380, 70, "red", 30, 30),
+//         creep2 = new Thing(creepImage2, 150, 350, "blue", 30, 30),
+//         creep3 = new Thing(creepImage2, 250, 150, "green", 30, 30),
+//         creep4 = new Thing(creepImage2, 360, 400, "gray", 30, 30),
+//         creep5 = new Thing(creepImage2, 650, 150, "hotpink", 30, 30),
+//         creep6 = new Thing(creepImage2, 700, 430, "orange", 30, 30),
+//         creep7 = new Thing(creepImage2, 490, 300, "yellow", 30, 30)
+//         ] 
+//         let walls = [
+//         wall1 = new WallThing(0, 200, "black", 180, 50),
+//         wall2 = new WallThing(640, 350, "black", 50, 180),
+//         wall3 = new WallThing(700, 250, "black", 180, 50),
+//         wall4 = new WallThing(300, 200, "black",50, 300),
+//         wall5 = new WallThing(500, 0, "black", 50, 250)
+//         ] 
+//         let finishLine = new WallThing(700, 0, "gray", 200, 100);
+//         gameLoop();
+//     }
+// }
+// theGame();
+// let runGame = setInterval(gameLoop, 100);
